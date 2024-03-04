a7ab27a0a8e1c66b025926773166b79bfbfa50dc
nemofalcon <nemo.falcon96@gmail.com>
2024-03-04 11:19:11 +0500

    change  layouts

--- src/app/environmens/environment.ts
+++ src/app/environmens/environment.ts
@@ -1,10 +1,10 @@
 export const env = {
   production: false,
-  apiUrl: 'https://merchant.tirgo.io/api/v1',
-  adminUrl: 'http://65.1.134.189/api/v2',
-  references: 'http://65.1.134.189/api/v2',
-  merchantUrl: 'http://65.1.134.189/api/v2',
-  orderApiUrl: 'http://65.1.134.189/api/v2',
+  apiUrl: 'https://test-merchant.tirgo.io/api/v1',
+  adminUrl: 'https://test-admin.tirgo.io/api/v2',
+  references: 'https://test-admin.tirgo.io/api/v2',
+  merchantUrl: 'https://test-admin.tirgo.io/api/v2',
+  orderApiUrl: 'https://test-admin.tirgo.io/api/v2',
 };
 // 'http://192.168.1.218:3003/api/v2'
 // 'https://merchant.tirgo.io/api/v1'
