2a401f461ca8279b42fa629be3c26a670c36abc6
fazliddinnarkhajayev <fazliddinnarkhajayev@gmail.com>
2024-04-04 12:24:47 +0500

    fixed-formcontrolname

--- src/app/modules/drivers/components/add-transport/add-transport.component.ts
+++ src/app/modules/drivers/components/add-transport/add-transport.component.ts
@@ -279,6 +279,7 @@ export class AddTransportComponent implements OnInit {
   selectFile(event: any, name: string) {
     const file: File = event.target.files[0];
     if (file) {
+      console.log(name, this.form.get(name)?.value, file)
       this.formData.append(name, file, new Date().getTime().toString() + '.jpg');
       const reader = new FileReader();
       reader.onload = () => {
@@ -429,8 +430,9 @@ export class AddTransportComponent implements OnInit {
     // transportFrontFilePath: string;
     // goodsTransportationLicenseCardFilePath: string;
     // driverLicenseFilePath: string;
-    // passportFilePath: string;
+    // passportFilePath: string; techPassportBackFilePath
     const uniqueFormData = removeDuplicateKeys(this.formData);
+    console.log(this.form.value)
     if (this.form.valid) {
       if (this.form.value.id) {
         this._driverService.updateTransport(uniqueFormData)
