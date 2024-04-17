bf615fab34894a5d7ac8fa76cf01096ba3bde032
shonasreeetdinov <najmiddinshonasriddinov@gmail.com>
2024-04-17 10:32:20 +0500

    create transaction

---
+++ src/app/modules/merchant/components/create-transaction/create-transaction.component.ts
@@ -0,0 +1,73 @@
+import { CurrencyPipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslocoModule } from '@ngneat/transloco';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { Inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { HeaderTextComponent } from 'app/shared/components/header-text/header-text.component';
+import { ToastrService } from 'ngx-toastr';
+import { MatSelectModule } from '@angular/material/select';
+import { NgxMatIntlTelInputComponent } from 'ngx-mat-intl-tel-input';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MerchantService } from '../../services/merchant.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { TypesService } from 'app/shared/services/types.service';
+
+@Component({
+    selector: 'app-create-transaction',
+    templateUrl: './create-transaction.component.html',
+    styleUrls: ['./create-transaction.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+    imports: [TranslocoModule, NgClass, NgxMatSelectSearchModule, MatProgressSpinnerModule, MatRadioModule, MatDatepickerModule, NgxMatIntlTelInputComponent, MatInputModule, MatIconModule, MatSelectModule, MatButtonModule, ReactiveFormsModule, MatDialogModule, FormsModule, NgFor, NgIf, MatTableModule, NgClass, CurrencyPipe, MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatMenuModule, MatSlideToggleModule, HeaderTextComponent],
+})
+export class CreateTransactionComponent {
+    currencies: any;
+    transactionType: string = '';
+    form: FormGroup = new FormGroup({
+        transactionType: new FormControl('', [Validators.required]),
+        amount: new FormControl('', [Validators.required]),
+        currencyId: new FormControl('', [Validators.required]),
+        comment: new FormControl(''),
+    })
+
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        private _toaster: ToastrService,
+        private _merChantService: MerchantService,
+        private _dialog: MatDialog,
+        private typesService: TypesService) {
+
+        this.typesService.getCurrencies().subscribe((res: any) => {
+            this.currencies = res.data;
+            this.form.patchValue({
+                currencyId: this.currencies[0].id,
+            })
+        })
+    }
+
+    create() {
+        this.form.disable();
+        console.log(this.form.value);
+        
+        this.form.enable()
+    }
+
+    closeModal() {
+        this._dialog.closeAll();
+    }
+
+}
+
